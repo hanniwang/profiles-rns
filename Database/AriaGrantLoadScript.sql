4033d2d48be4bf426122a799e180ef3125ac73c7
samangan <harry.hull1@gmail.com>
2014-01-30 13:20:38 -0600

    fix aria grant loading script

--- Database/AriaGrantLoadScript.sql
+++ Database/AriaGrantLoadScript.sql
@@ -31,6 +31,6 @@ inner join [DevDataRepo].[dbo].[AriaGrantRole] B on A.ARIARECORDID=B.ARIARecordI
 inner join [DevDataRepo].[dbo].[ARIAGrant] X on B.ARIARecordID=X.ARIARecordID
 inner join [ProfilesRNS].[User.Account].[User] C on B.SAPID=RIGHT('00000000'+ISNULL(C.internalusername,''),8)
 where X.ProjectStatus='Awarded'
-and B.Role='Principal Investigator' 
 and B.SAPID  <> 'Non-UAMS'
+and C.PersonID IS NOT NULL
 GO
