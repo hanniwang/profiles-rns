b25b7dacf1ce2d7048d09b14abdd34725a38c42b
Shariq Tariq <satariq@uams.edu>
2014-03-06 20:20:46 +0000

    Profile.Data.LoadGrantsData.StoredProcedure.sql edited online with Bitbucket

--- Database/Schema/Profile.Data.LoadGrantsData.StoredProcedure.sql
+++ Database/Schema/Profile.Data.LoadGrantsData.StoredProcedure.sql
@@ -1,3 +1,14 @@
+USE [ProfilesRNS]
+GO
+
+/****** Object:  StoredProcedure [Profile.Data].[LoadGrantsData]    Script Date: 03/06/2014 14:19:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
 CREATE procedure [Profile.Data].[LoadGrantsData]
 AS 
     BEGIN
@@ -12,7 +23,7 @@ AS
                 
             -- truncate table
             truncate table [Profile.Data].[Grant.Information]; 
-            truncate table [Profile.Data].[Grant.AffiliatedPeople];
+            delete from [Profile.Data].[Grant.AffiliatedPeople] where  (Excluded is null or Excluded='0');
             
             INSERT INTO [Profile.Data].[Grant.Information] (
 	      [ARIAGrantID],
@@ -31,16 +42,21 @@ AS
 	      [GrantID],
 	      [PersonID],
 	      [SAPID],
-	      [IsPrincipalInvestigator]
+	      [IsPrincipalInvestigator],
+	      [Excluded]
 	    )                                              
 	    select A.GrantID,C.PersonId, B.SAPID,
-	    (SELECT CASE WHEN B.Role='Principal Investigator' THEN 1 ELSE 0 END)  from [Profile.Data].[Grant.Information] A
+	    (SELECT CASE WHEN B.Role='Principal Investigator' THEN 1 ELSE 0 END),'0'  from [Profile.Data].[Grant.Information] A
 	    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrantRole] B on A.ARIARECORDID=B.ARIARecordID
 	    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] X on B.ARIARecordID=X.ARIARecordID
 	    inner join [ProfilesRNS].[User.Account].[User] C on B.SAPID=RIGHT('00000000'+ISNULL(C.internalusername,''),8)
 	    where X.ProjectStatus='Awarded'
 	    and B.SAPID  <> 'Non-UAMS'
 	    and C.PersonID IS NOT NULL;
+	    
+	    with a as
+	   (select ROW_NUMBER() over (PARTITION by GrantId, PersonId, SapID, IsPrincipalInvestigator order by excluded desc) as rownum, *
+	    from [Profile.Data].[Grant.AffiliatedPeople]) delete From a where rownum > 1;
 
           COMMIT;    
         END TRY
@@ -56,4 +72,6 @@ AS
             RAISERROR(@ErrMsg, @ErrSeverity, 1)
         END CATCH	            
     END;
+
+
 GO
