4286d39aea2e0cd16a5765139a3523416f4650d3
Shariq Tariq <satariq@uams.edu>
2014-03-13 18:20:35 +0000

    Profile.Data.LoadGrantsData.StoredProcedure.sql edited online with Bitbucket
    Driving it off the project id in the grants table

--- Database/Schema/Profile.Data.LoadGrantsData.StoredProcedure.sql
+++ Database/Schema/Profile.Data.LoadGrantsData.StoredProcedure.sql
@@ -1,19 +1,14 @@
 USE [ProfilesRNS]
 GO
 
-/****** Object:  StoredProcedure [Profile.Data].[LoadGrantsData]    Script Date: 03/07/2014 02:46:16 ******/
+/****** Object:  StoredProcedure [Profile.Data].[LoadGrantsData]    Script Date: 03/07/2014 02:16:33 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-
-
-CREATE procedure [Profile.Data].[LoadGrantsData] (@REFRESH BIT=0 )
+ALTER procedure [Profile.Data].[LoadGrantsData] (@REFRESH BIT=0 )
 AS 
     BEGIN
         SET NOCOUNT ON;	
@@ -37,11 +32,12 @@ AS
 		      [EndDate],
 		      [GrantTitle],
 		      [GrantAmount],
+		      [PRJID],
 		      [IsActive]
 		    )
-		    select [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, 1 
+		    select [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, [ARIAGrant].PRN, 1 
 		    From ( 
-		    select ROW_NUMBER() over (partition by Title order by ARIARecordID desc, StopDate desc) as rownumber, * 
+		    select ROW_NUMBER() over (partition by PRN order by ARIARecordID desc, StopDate desc) as rownumber, * 
 		    from  [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] 
 		    where ProjectStatus = 'Awarded') ARIAGrant 
 		    where rownumber < 2;
@@ -55,8 +51,8 @@ AS
 		    )                                              
 		    select A.GrantID,C.PersonId, B.SAPID,
 		    (SELECT CASE WHEN B.Role='Principal Investigator' THEN 1 ELSE 0 END),'0'  from [Profile.Data].[Grant.Information] A
-		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrantRole] B on A.ARIARECORDID=B.ARIARecordID
-		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] X on B.ARIARecordID=X.ARIARecordID
+		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrantRole] B on A.ARIARECORDID=B.ARIARecordID and A.PRJID=B.PRN
+		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] X on B.ARIARecordID=X.ARIARecordID and A.PRJID=X.PRN
 		    inner join [ProfilesRNS].[User.Account].[User] C on B.SAPID=RIGHT('00000000'+ISNULL(C.internalusername,''),8)
 		    where X.ProjectStatus='Awarded'
 		    and B.SAPID  <> 'Non-UAMS'
@@ -79,12 +75,13 @@ AS
 		      [EndDate],
 		      [GrantTitle],
 		      [GrantAmount],
+		      [PRJID],
 		      [IsActive]
 		    )
-		    SELECT [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, 1
+		    SELECT [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, [ARIAGrant].PRN,1
 		    FROM [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] ARIAGrant
 		    inner join [Profile.Data].[Grant.Information] GrantInformation 
-		    on ARIAGrant.Title=GrantInformation.GrantTitle
+		    on ARIAGrant.PRN=GrantInformation.PRJID
 		    and ARIAGrant.ARIARecordID > GrantInformation.ARIARecordID 
 		    WHERE [ARIAGrant].ProjectStatus = 'Awarded';
 		    
@@ -96,18 +93,19 @@ AS
 		      [EndDate],
 		      [GrantTitle],
 		      [GrantAmount],
+		      [PRJID],
 		      [IsActive]
 		    )
-		    SELECT [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, 1
+		    SELECT [ARIAGrant].ARIAGrantID, [ARIAGrant].ARIARecordID, [ARIAGrant].StartDate, [ARIAGrant].StopDate, [ARIAGrant].Title, [ARIAGrant].TotalAmount, [ARIAGrant].PRN, 1
 		    FROM [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] ARIAGrant
 		    WHERE NOT EXISTS  
 		    (select * from [Profile.Data].[Grant.Information] GrantInformation
-		    where ARIAGrant.Title=GrantInformation.GrantTitle)
+		    where ARIAGrant.PRN=GrantInformation.PRJID)
 		    AND [ARIAGrant].ProjectStatus = 'Awarded';
 		    
 		    -- Remove duplicate values of grants
 		    with a as
-		    (select ROW_NUMBER() over (PARTITION by GrantTitle order by ARIARecordID desc, EndDate desc) as rownum, *
+		    (select ROW_NUMBER() over (PARTITION by PRJID order by ARIARecordID desc, EndDate desc) as rownum, *
 		    from [Profile.Data].[Grant.Information]) delete from a where rownum > 1;
 
 		    INSERT INTO [Profile.Data].[Grant.AffiliatedPeople] (
@@ -119,8 +117,8 @@ AS
 		    )                                              
 		    select A.GrantID,C.PersonId, B.SAPID,
 		    (SELECT CASE WHEN B.Role='Principal Investigator' THEN 1 ELSE 0 END),'0'  from [Profile.Data].[Grant.Information] A
-		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrantRole] B on A.ARIARECORDID=B.ARIARecordID
-		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] X on B.ARIARecordID=X.ARIARecordID
+		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrantRole] B on A.ARIARECORDID=B.ARIARecordID and A.PRJID=B.PRN
+		    inner join [HOSP_SQL1].[FacFac].[dbo].[vAriaGrant] X on B.ARIARecordID=X.ARIARecordID and A.PRJID=X.PRN
 		    inner join [ProfilesRNS].[User.Account].[User] C on B.SAPID=RIGHT('00000000'+ISNULL(C.internalusername,''),8)
 		    where X.ProjectStatus='Awarded'
 		    and B.SAPID  <> 'Non-UAMS'
@@ -132,7 +130,7 @@ AS
 		    from [Profile.Data].[Grant.AffiliatedPeople]) delete From a where rownum > 1;
 		  END;
           COMMIT; 
-          EXEC  [Profile.Data].[Grant.Entity.UpdateEntityAllPersons];
+        EXEC  [Profile.Data].[Grant.Entity.UpdateEntityAllPersons];
         END TRY
         BEGIN CATCH
 			--Check success
@@ -146,6 +144,5 @@ AS
             RAISERROR(@ErrMsg, @ErrSeverity, 1)
         END CATCH	            
     END;
-
 
 GO
